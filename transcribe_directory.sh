3cce369abb78bdb9959e054c328e94fc40d9c752
Aleksei Prishchepo <3372215@gmail.com>
2023-10-12 20:58:40 +0000

    Removed credentials from container

--- transcribe_directory.sh
+++ transcribe_directory.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-  then
-    echo "No DIR_PATH supplied."
-    exit 1
+# Provide records directory path in the first argument
+if [ -z "$1" ]; then
+  echo "Supply records directory path in the argument."
+  exit 1
 fi
 
+# Path to credentials.ini
 source <(grep = ./sample/credentials.ini)
 
-python ./sample/transcribe_directory.py $1 $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY $STORAGE_BUCKET_NAME $TRANSCRIBE_API_KEY "md5"
+# 'md5' at the end tells script to name html files using hash from the name of the audio file
+# delete it if you want to name html files after audio files
+docker run -i --rm -v $1:/mnt/Records speechkitty /bin/bash -c "python \
+./sample/transcribe_directory.py /mnt/Records $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY \
+$STORAGE_BUCKET_NAME $TRANSCRIBE_API_KEY md5"
