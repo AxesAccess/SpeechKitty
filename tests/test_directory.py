c279dba14901f907fc6f966165e9255ae21e2ddb
Aleksei Prishchepo <3372215@gmail.com>
2023-11-12 01:42:24 +0300

    Tests reflect broader regexp template

--- tests/test_directory.py
+++ tests/test_directory.py
@@ -7,14 +7,14 @@ class TestDirectory(unittest.TestCase):
         self.directory = Directory(path="sample/records")
 
     def test_get_records(self):
-        assert len(self.directory.get_records(skip_processed=False)) == 3
+        assert len(self.directory.get_records(skip_processed=False)) == 4
 
     def test_get_records_skip(self):
         assert len(self.directory.get_records(skip_processed=True)) == 2
 
     def test_get_records_exclude(self):
         exclude = "^.+(?:-in|-out)\\.wav"
-        assert len(self.directory.get_records(regexp_exclude=exclude, skip_processed=False)) == 1
+        assert len(self.directory.get_records(regexp_exclude=exclude, skip_processed=False)) == 2
 
     def test_get_records_skip_exclude(self):
         exclude = "^.+(?:-in|-out)\\.wav"
